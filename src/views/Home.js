d667929efcc5f9cd3320acc2f1ed72ba5f23693e
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-29 13:00:09 -0700

    Home view fetches backend locally

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+const fetch = require('cross-fetch');
 
 export default function Home() {
-  return (
-    <div>Sharks</div>
-  )
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(`http://localhost:7891/sharks`);
+      console.log(data);
+    };
+    load();
+  }, []);
+
+  return <div>SHARKS RUNNING!</div>;
 }
