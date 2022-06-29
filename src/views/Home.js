9fbe42c1659012c5b61bfcd913657eb3640d8ed2
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-29 14:21:36 -0700

    fixed port, jsonified response data, validated return

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-const fetch = require('cross-fetch');
+// const fetch = require('cross-fetch');
 
 export default function Home() {
   useEffect(() => {
     const load = async () => {
-      const data = await fetch(`http://localhost:7891/sharks`);
+      const resp = await fetch(`http://localhost:7890/sharks`);
+      const data = await resp.json();
       console.log(data);
     };
     load();
